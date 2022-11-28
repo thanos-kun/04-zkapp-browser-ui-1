51b7b536d173cd6ec39cc1529b1aff8c9c691405
Muhammad Bukhori <mbukhori497@gmail.com>
2022-11-29 01:50:13 +0700

    Deploy gh-pages

--- ui/out/_next/static/Ama4bITm2ZmOg5T6w0Axg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-901f492c89e8d5e0.js","static\u002Fcss\u002F622f4846eff8cf1a.css"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-75f8c62d1b846d4f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
