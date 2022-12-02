3f3c4a6120f559adc83c5aa5a3e7efd33f8a57c8
Muhammad Bukhori <mbukhori497@gmail.com>
2022-12-02 17:17:18 +0700

    update

--- ui/pages/_app.page.tsx
+++ ui/pages/_app.page.tsx
@@ -120,6 +120,69 @@ export default function App() {
 	const el6 = document.getElementById("status6")!
 	el6.style.display = "block";
   }
+  
+   const send1 = () => {
+    const el = document.getElementById("send1")!
+	el.style.display = "block";
+	const el2 = document.getElementById("send2")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("send3")!
+	el3.style.display = "none";
+	const el4 = document.getElementById("send4")!
+	el4.style.display = "none";
+  }
+  
+  const send2 = () => {
+    const el = document.getElementById("send1")!
+	el.style.display = "none";
+	const el2 = document.getElementById("send2")!
+	el2.style.display = "block";
+	const el3 = document.getElementById("send3")!
+	el3.style.display = "none";
+	const el4 = document.getElementById("send4")!
+	el4.style.display = "none";
+  }
+  
+  const send3 = () => {
+    const el = document.getElementById("send1")!
+	el.style.display = "none";
+	const el2 = document.getElementById("send2")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("send3")!
+	el3.style.display = "block";
+	const el4 = document.getElementById("send4")!
+	el4.style.display = "none";
+  }
+  
+  const send4 = () => {
+    const el = document.getElementById("send1")!
+	el.style.display = "none";
+	const el2 = document.getElementById("send2")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("send3")!
+	el3.style.display = "none";
+	const el4 = document.getElementById("send4")!
+	el4.style.display = "block";
+  }
+  
+  const donesend = () => {
+    const el = document.getElementById("send1")!
+	el.style.display = "none";
+	const el2 = document.getElementById("send2")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("send3")!
+	el3.style.display = "none";
+	const el4 = document.getElementById("send4")!
+	el4.style.display = "none";
+	const el5 = document.getElementById("sendLoading")!
+	el5.style.display = "none";
+	const el6 = document.getElementById("sendDone")!
+	el6.style.display = "block";
+	const el7 = document.getElementById("sendCheck")!
+	el7.style.display = "block";
+	const el8 = document.getElementById("closeSend")!
+	el8.style.display = "block";
+  }
 	
   // -------------------------------------------------------
   // Do Setup
@@ -139,9 +202,7 @@ export default function App() {
 
         if (mina == null) {
           setState({ ...state, hasWallet: false });
-		  status4();
-          return;
-		  
+		  return;
         }
 
         const publicKeyBase58 : string = (await mina.requestAccounts())[0];
@@ -182,32 +243,162 @@ export default function App() {
       }
   };
 
-
   // -------------------------------------------------------
-  // Wait for account to exist, if it didn't
-
-  useEffect(() => {
-    (async () => {
-      if (state.hasBeenSetup && !state.accountExists) {
-        for (;;) {
-          console.log('checking if account exists...');
-          const res = await state.zkappWorkerClient!.fetchAccount({ publicKey: state.publicKey! })
-          const accountExists = res.error == null;
-          if (accountExists) {
-            break;
-          }
-          await new Promise((resolve) => setTimeout(resolve, 5000));
-        }
-        setState({ ...state, accountExists: true });
-      }
-    })();
-  }, [state.hasBeenSetup]);
-
+  // Newwwwww
+  const connectBtnclick = () => {
+    const el = document.getElementById("loadingBtn")!
+	el.style.display = "block";
+	const el2 = document.getElementById("connectBtn")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("loading")!
+	el3.style.display = "block";
+	const el4 = document.getElementById("banner")!
+	el4.style.display = "none";
+  };
+  
+  const hideloadingBtn = () => {
+    const el = document.getElementById("loadingBtn")!
+	el.style.display = "none";
+	const el2 = document.getElementById("loading")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("succes")!
+	el3.style.display = "block";
+  };
+  
+  const closeGetclick = () => {
+    const el = document.getElementById("getscreen")!
+	el.style.display = "none";
+	const el2 = document.getElementById("getBtnDisable")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("getBtn")!
+	el3.style.display = "block";
+	const el4 = document.getElementById("sendBtnDisable")!
+	el4.style.display = "none";
+	const el5 = document.getElementById("sendBtn")!
+	el5.style.display = "block";
+  };
+  
+  const closeSendclick = () => {
+    const el = document.getElementById("sendscreen")!
+	el.style.display = "none";
+	const el2 = document.getElementById("sendBtnDisable")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("sendBtn")!
+	el3.style.display = "block";
+	const el4 = document.getElementById("getBtnDisable")!
+	el4.style.display = "none";
+	const el5 = document.getElementById("getBtn")!
+	el5.style.display = "block";
+	const el6 = document.getElementById("sendDone")!
+	el6.style.display = "none";
+    const el7 = document.getElementById("sendCheck")!
+	el7.style.display = "none";
+    const el8 = document.getElementById("closeSend")!
+	el8.style.display = "none";
+  };
+  
+  const getscreenShow = () => {
+	const el = document.getElementById("getscreen")!
+	el.style.display = "block";
+	const el2 = document.getElementById("getBtn")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("getBtnDisable")!
+	el3.style.display = "block";
+	const el4 = document.getElementById("sendBtnDisable")!
+	el4.style.display = "block";
+	const el5 = document.getElementById("sendBtn")!
+	el5.style.display = "none";
+  };
+  
+  const sendscreenShow = () => {
+	const el = document.getElementById("sendscreen")!
+	el.style.display = "block";
+	const el2 = document.getElementById("sendBtn")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("sendBtnDisable")!
+	el3.style.display = "block";
+	const el4 = document.getElementById("getBtnDisable")!
+	el4.style.display = "block";
+	const el5 = document.getElementById("getBtn")!
+	el5.style.display = "none";
+    const el6 = document.getElementById("sendLoading")!
+	el6.style.display = "block";
+  };
+  
+  const noAccount = () => {
+    const el = document.getElementById("loadingBtn")!
+	el.style.display = "none";
+    const el2 = document.getElementById("caution")!
+	el2.style.display = "block";
+    const el3 = document.getElementById("ftxt")!
+	el3.style.display = "block";
+	const el4 = document.getElementById("backNoAccount")!
+	el4.style.display = "block";
+    const el5 = document.getElementById("loading")!
+	el5.style.display = "none";
+  }
+  
+  const backNoAccountClick = () => {
+    location.reload();
+  }
+  
+  const backNoWalletClick = () => {
+    const el = document.getElementById("caution")!
+	el.style.display = "none";
+	const el2 = document.getElementById("backNoWallet")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("connectBtn")!
+	el3.style.display = "block";
+	const el4 = document.getElementById("banner")!
+	el4.style.display = "block";
+	const el5 = document.getElementById("walletLink")!
+	el5.style.display = "none";
+    const el6 = document.getElementById("walletTxt")!
+	el6.style.display = "none";
+	const el7 = document.getElementById("status4")!
+	el7.style.display = "none";
+  }
+  
+  const noWallet = () => {
+    const el = document.getElementById("loadingBtn")!
+	el.style.display = "none";
+    const el2 = document.getElementById("caution")!
+	el2.style.display = "block";
+    const el3 = document.getElementById("walletTxt")!
+	el3.style.display = "block";
+	const el4 = document.getElementById("backNoWallet")!
+	el4.style.display = "block";
+    const el5 = document.getElementById("loading")!
+	el5.style.display = "none";
+	const el6 = document.getElementById("walletLink")!
+	el6.style.display = "block";
+  }
+  
+  const getload = () => {
+    const el = document.getElementById("getLoading")!
+	el.style.display = "block";
+	const el2 = document.getElementById("gettext")!
+	el2.style.display = "none";
+	const el3 = document.getElementById("getnumber")!
+	el3.style.display = "none";
+  }
+  
+  const getdone = () => {
+    const el = document.getElementById("getLoading")!
+	el.style.display = "none";
+	const el2 = document.getElementById("gettext")!
+	el2.style.display = "block";
+	const el3 = document.getElementById("getnumber")!
+	el3.style.display = "block";
+  }
   // -------------------------------------------------------
+ 
+   // -------------------------------------------------------
   // Send a transaction
 
   const onSendTransaction = async () => {
     setState({ ...state, creatingTransaction: true });
+	send1();
     console.log('sending a transaction...');
 
     await state.zkappWorkerClient!.fetchAccount({ publicKey: state.publicKey! });
@@ -215,12 +406,15 @@ export default function App() {
     await state.zkappWorkerClient!.createUpdateTransaction();
 
     console.log('creating proof...');
+	send2();
     await state.zkappWorkerClient!.proveUpdateTransaction();
 
     console.log('getting Transaction JSON...');
+	send3();
     const transactionJSON = await state.zkappWorkerClient!.getTransactionJSON()
 
     console.log('requesting send transaction...');
+	send4();
     const { hash } = await (window as any).mina.sendTransaction({
       transaction: transactionJSON,
       feePayer: {
@@ -233,103 +427,54 @@ export default function App() {
       'See transaction at https://berkeley.minaexplorer.com/transaction/' + hash
     );
 
+	donesend();
+
     setState({ ...state, creatingTransaction: false });
   }
-
-  // -------------------------------------------------------
+  
+   // -------------------------------------------------------
   // Refresh the current state
 
   const onRefreshCurrentNum = async () => {
     console.log('getting zkApp state...');
+	getload();
     await state.zkappWorkerClient!.fetchAccount({ publicKey: state.zkappPublicKey! })
     const currentNum = await state.zkappWorkerClient!.getNum();
     console.log('current state:', currentNum.toString());
+	getdone();
 
     setState({ ...state, currentNum });
   }
-
-	const faucetLink = "https://faucet.minaprotocol.com/?address=" + state.publicKey!.toBase58();
-  
-  // -------------------------------------------------------
-  // Create UI elements
-
+ 
   let hasWallet;
   if (state.hasWallet != null && !state.hasWallet) {
     const auroLink = 'https://www.aurowallet.com/';
-    const auroLinkElem = <a href={auroLink} target="_blank" rel="noreferrer"> [Link] </a>
-    hasWallet = <div> Could not find a wallet. Install Auro wallet here: { auroLinkElem }</div> 
+    hasWallet = <a id="walletLink" style={{display: 'block'}} href={auroLink} target="_blank" rel="noreferrer">
+		<h1 className={styles.walletLink}>[[CLICK HERE]]</h1>
+	</a>
+	status4();
+	noWallet();
   }
 
   let setupText = state.hasBeenSetup ? 'SnarkyJS Ready' : 'Loading...';
   let setup = <div id="setup" style={{display: 'block'}}> { setupText } { hasWallet }</div>
-
-
   
-  // -------------------------------------------------------
-  // Newwwwww
-  const connectBtnclick = () => {
-    const el = document.getElementById("loadingBtn")!
-	el.style.display = "block";
-	
-	const el2 = document.getElementById("connectBtn")!
-	el2.style.display = "none";
-	
-	const el3 = document.getElementById("loading")!
-	el3.style.display = "block";
-  };
-  
-  const hideloadingBtn = () => {
-    const el = document.getElementById("loadingBtn")!
-	el.style.display = "none";
-  };
-  
-  const closeBtnclick = () => {
-    const el = document.getElementById("getscreen")!
-	el.style.display = "none";
-	
-	const el2 = document.getElementById("getBtnDisable")!
-	el2.style.display = "none";
-	
-	const el3 = document.getElementById("getBtn")!
-	el3.style.display = "block";
-  };
-  
-  const getscreenShow = () => {
-	const el3 = document.getElementById("getBtn")!
-	el3.style.display = "none";
-	
-    const el = document.getElementById("getscreen")!
-	el.style.display = "block";
-	
-	const el2 = document.getElementById("getBtnDisable")!
-	el2.style.display = "block";
-  };
-  
-  const noAccount = () => {
-    const el = document.getElementById("loadingBtn")!
-	el.style.display = "none";
-    const el2 = document.getElementById("caution")!
-	el2.style.display = "block";
-    const el3 = document.getElementById("ftxt")!
-	el3.style.display = "block";
-    const el4 = document.getElementById("flink")!
-	el4.style.display = "block";
-    const el5 = document.getElementById("loading")!
-	el5.style.display = "none";
-  }
-  // -------------------------------------------------------
- 
   let accountDoesNotExist;
   if (state.hasBeenSetup && !state.accountExists) {
+	  const faucetLink = "https://faucet.minaprotocol.com/?address=" + state.publicKey!.toBase58();
+	accountDoesNotExist = <a id="flink" style={{display: 'block'}} href={faucetLink} target="_blank" rel="noreferrer">
+		<h1 className={styles.faucetHere}>[[CLICK HERE]]</h1>
+	</a>
 	status5();
 	noAccount();
+	hasBeenSetup: false;
   }
   
   let mainContent;
   if (state.hasBeenSetup && state.accountExists) {	
     mainContent =
 		<div>
-			<a id="sendBtn" style={{display: 'block'}} onClick={() => {onSendTransaction(); }}>
+			<a id="sendBtn" style={{display: 'block'}} onClick={() => {onSendTransaction(); sendscreenShow(); }}>
 					<span className={styles.sendBtn}> </span>
 			</a>
 			
@@ -338,20 +483,43 @@ export default function App() {
 			</a>
 			
 			<span id="getBtnDisable" style={{display: 'none'}} className={styles.getBtnDisable}></span>
+			<span id="sendBtnDisable" style={{display: 'none'}} className={styles.sendBtnDisable}></span>
 			
 			<h1 className={styles.txtAddrs}>{  state.publicKey!.toBase58() } </h1>
 			<h1 className={styles.addrs}>Address :</h1>
 			
 			<div id="getscreen" style={{display: 'none'}} className={styles.getscreen}>
-				<span className={styles.screenBlack}> </span>
+				<span className={styles.getscreenBlack}> </span>
 				<span className={styles.getscreenImg}> </span>
 				
-				<a id="closeBtn" style={{display: 'block'}} onClick={() => {closeBtnclick(); }}>
-					<span className={styles.closeBtn}> </span>
+				<a id="closeGet" style={{display: 'block'}} onClick={() => {closeGetclick(); }}>
+					<span className={styles.closeGet}> </span>
 				</a>
 				
-				<h1 className={styles.txtState}>Current Number in ZkApp :</h1>
-				<h1 className={styles.numState}>{ state.currentNum!.toString() } </h1>
+				<span id="getLoading" style={{display: 'none'}} className={styles.getLoading}> </span>
+				
+				<h1 id="gettext" style={{display: 'none'}} className={styles.txtState}>Current Number in ZkApp :</h1>
+				<h1 id="getnumber" style={{display: 'none'}} className={styles.numState}>{ state.currentNum!.toString() } </h1>
+			</div>
+			
+			<div id="sendscreen" style={{display: 'none'}} className={styles.sendscreen}>
+				<span className={styles.sendscreenBlack}> </span>
+				<span className={styles.sendscreenImg}> </span>
+				
+				<a id="closeSend" style={{display: 'none'}} onClick={() => {closeSendclick(); }}>
+					<span className={styles.closeSend}> </span>
+				</a>
+				
+				<span id="sendLoading" style={{display: 'none'}} className={styles.sendLoading}> </span>
+				<span id="sendCheck" style={{display: 'none'}} className={styles.sendCheck}> </span>
+				<span id="sendDone" style={{display: 'none'}} className={styles.sendDone}> </span>
+				
+				<h1 id="send1" style={{display: 'none'}} className={styles.statusSendTxt}>Sending a Transaction...</h1>
+				<h1 id="send2" style={{display: 'none'}} className={styles.statusSendTxt}>Creating Proof...</h1>
+				<h1 id="send3" style={{display: 'none'}} className={styles.statusSendTxt}>Getting Transaction JSON...</h1>
+				<h1 id="send4" style={{display: 'none'}} className={styles.statusSendTxt}>Requesting Send Transaction...</h1>
+
+
 			</div>
 
 		</div>
@@ -364,7 +532,7 @@ export default function App() {
 	  <Head>
         <title>zkApp [mbukhori]</title>
         <meta name="description" content="ZkApp By mbukhori" />
-        <link rel="icon" type="image/x-icon" href="" />
+        <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAADlkvAAAAAAALuuvQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAISESEREiIhAhIRIhERERESEhEiIRIiIQISESIREREREhIRIRESIiEBERERERERERABAAEQEQABABEBAQEBAREQEQEBAQEAEQARAQEBAQEREBEAAQEBAAEBERERERERERAQEBAQEBAQERERERERERERERERERERERERERERERERFbwgAAWf8AAFjCAABZ/wAAW8IAAP//AAAjYgAAaq8AAGqmAABqrwAAYqIAAP//AABVVQAA//8AAP//AAD//wAA" rel="icon" type="image/x-icon" />
 	  </Head>
 	  
 		<main className={styles.main}>
@@ -379,20 +547,33 @@ export default function App() {
 					<span id="loading" style={{display: 'none'}} className={styles.loading}> </span>
 					
 					<h1 id="status1" style={{display: 'none'}} className={styles.statusTxt}>Status : Sync & Checking Wallet ...</h1>
-					<h1 id="status2" style={{display: 'none'}} className={styles.statusTxt}>Status : Sync DONE! Login to Wallet...</h1>
-					<h1 id="status3" style={{display: 'none'}} className={styles.statusTxt}>Status : Checking & Validasi Address...</h1>
+					<h1 id="status2" style={{display: 'none'}} className={styles.statusTxt}>Status : Sync DONE! Connect to Wallet...</h1>
+					<h1 id="status3" style={{display: 'none'}} className={styles.statusTxt}>Status : Checking & Validation Address...</h1>
 					<h1 id="status4" style={{display: 'none'}} className={styles.statusTxt}>Status : Wallet Extension Not Found!</h1>
 					<h1 id="status5" style={{display: 'none'}} className={styles.statusTxt}>Status : Account Address Not Valid or No Balance!</h1>
 					<h1 id="status6" style={{display: 'none'}} className={styles.statusTxt}>Status : READY FOR TRANSACTION!!!</h1>
 					
 					<span id="caution" style={{display: 'none'}} className={styles.caution}> </span>
+					
+					<span id="succes" style={{display: 'none'}} className={styles.succes}> </span>
 
-					<h1 id="ftxt" style={{display: 'none'}} className={styles.faucetTxt}>Address Not Valid or No Balance!! Please check and fund on this link >>> </h1>
-					<a id="flink" style={{display: 'none'}} href={faucetLink} target="_blank" rel="noreferrer">
-					<h1 className={styles.faucetHere}>[[CLICK HERE]]</h1>
+					<h1 id="ftxt" style={{display: 'none'}} className={styles.faucetTxt}>Invalid Account or No Balance!! Please check and fund on this link </h1>
+					
+					<h1 id="walletTxt" style={{display: 'none'}} className={styles.walletTxt}>Could not find a wallet. Please Install Auro wallet and Re-Connect!! </h1>
+					
+					<a id="backNoAccount" style={{display: 'none'}} onClick={() => {backNoAccountClick(); }}>
+						<span className={styles.backNoAccount}> </span>
 					</a>
+					
+					<a id="backNoWallet" style={{display: 'none'}} onClick={() => {backNoWalletClick(); }}>
+						<span className={styles.backNoWallet}> </span>
+					</a>
+					
+					<span id="banner" style={{display: 'block'}} className={styles.banner}> </span>
+					
 				{mainContent}
 				{accountDoesNotExist}
+				{hasWallet}
 			</div>
 			
 
