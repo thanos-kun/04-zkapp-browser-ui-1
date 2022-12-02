3bb6734badd1d94d4f749fd7d3b4a1b087b113cd
Muhammad Bukhori <mbukhori497@gmail.com>
2022-12-03 01:11:32 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-4d0dc353316a0c25.js
+++ _next/static/chunks/webpack-4d0dc353316a0c25.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "e8fb5f5cdbb46a61" + ".css";
+/******/ 			return "static/css/" + "b0add2edbddd1643" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
